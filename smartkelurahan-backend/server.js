aa107beca1bd96aeac5713740dcbd91fdeabd535
Angelina0905 <yudriangell@gmail>
2026-05-12 23:24:41 +0700

    ubah bucket

--- smartkelurahan-backend/server.js
+++ smartkelurahan-backend/server.js
@@ -49,7 +49,7 @@ const s3 = new AWS.S3();
 const upload = multer({
     storage: multerS3({
         s3: s3,
-        bucket: process.env.S3_BUCKET,
+        bucket: process.env.S3_BUCKET_NAME,
         acl: 'public-read',
         key: function (req, file, cb) {
             cb(null, Date.now() + '-' + file.originalname);
