04fe5f28bb300bdce10c0bf02cdf7f50d61bffa2
Angelina0905 <yudriangell@gmail>
2026-05-12 12:56:37 +0700

    first commit

---
+++ smartkelurahan-backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mysql = require('mysql2');
+const multer = require('multer');
+const AWS = require('aws-sdk');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+db.connect(() => {
+    console.log('RDS Connected');
+});
+
+app.get('/', (req, res) => {
+    res.send('SmartKelurahan Backend Jalan');
+});
+
+app.listen(5000, () => {
+    console.log('Server running');
+});
