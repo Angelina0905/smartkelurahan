aa6cc7269eb38b37de443404aac3e22e4c9fade7
Angelina0905 <yudriangell@gmail>
2026-05-13 00:18:56 +0700

    harus jadi sih2

--- smartkelurahan-backend/server.js
+++ smartkelurahan-backend/server.js
@@ -115,7 +115,7 @@ app.get('/pengaduan', (req, res) => {
 // FRONTEND
 // =======================
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
